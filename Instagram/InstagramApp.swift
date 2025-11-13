263a51ffe249811b37f983b7bf8c6842e6b054e4
vahanm-dev <dev.vahanm@gmail.com>
2025-11-13 11:51:38 +0400

    Initial project setup

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Vahan Muradyan on 13.11.25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
